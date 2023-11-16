e82e8020c52449066a26ed11ad536686a2159d5b
joseantonioalvarez21 <145370788+joseantonioalvarez21@users.noreply.github.com>
2023-11-16 20:20:17 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+const botonAceptarCookies = document.getElementById('btn-aceptar-cookies');
+const avisoCookies = document.getElementById('aviso-cookies');
+const fondoAvisoCookies = document.getElementById('fondo-aviso-cookies');
+
+dataLayer = [];
+
+if(!localStorage.getItem('cookiesAceptadas')){
+	avisoCookies.classList.add('activo');
+	fondoAvisoCookies.classList.add('activo');
+} else {
+	dataLayer.push({'event': 'cookiesAceptadas'});
+}
+
+botonAceptarCookies.addEventListener('click', () => {
+	avisoCookies.classList.remove('activo');
+	fondoAvisoCookies.classList.remove('activo');
+
+	localStorage.setItem('cookiesAceptadas', true);
+
+	dataLayer.push({'event': 'cookiesAceptadas'});
+});
